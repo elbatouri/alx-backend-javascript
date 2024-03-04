fc4185b71daf6848a8213d3a7e3e72628aa0e494
el batouri rabi <elbatouri@gmail.com>
2024-03-04 22:01:01 +0000

    fix task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const ary = [];
   for (const val of array) {
-    ary.push(appendString + array[array.index(value)]);
+    ary.push(appendString + array[array.index(val)]);
   }
 
   return ary;
