62d6bb9e15d6951a1df239fee9e935e5a75af038
el batouri rabi <elbatouri@gmail.com>
2024-03-05 21:41:07 +0000

    task 6 pushing

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((resultat) => (
+      resultat.map((outcome) => ({
+        status: outcome.status,
+        value: outcome.status === 'fulfilled' ? outcome.value : String(outcome.reason),
+      }))
+    ));
+}
+
