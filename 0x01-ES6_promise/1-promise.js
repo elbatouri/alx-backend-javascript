60a769b84e4e2dc1c1f0a3e6be9a7ef34e84e7a0
el batouri rabi <elbatouri@gmail.com>
2024-03-05 21:11:27 +0000

    fix style  second file

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,11 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
-      // If 'success' is true, resolve the Promise with a success object.
       resolve({
-        status: 200,  // HTTP status code for success
-        body: 'Success',  // Success message or response body
+        status: 200,
+        body: 'Success',
       });
     } else {
-      // If 'success' is false, reject the Promise with an error object.
       reject(new Error('The fake API is not working currently'));
     }
   });
