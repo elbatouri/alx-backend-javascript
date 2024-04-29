75cc743536c0052ef0fdf724fd6e75c04469143a
elbatouri rabi <elbatouri@gmail.com>
2024-04-29 20:49:24 +0000

    create a function named displayMessage that prints in STDOUT

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message on the standard output (STDOUT).
+ * @param {string} msg The message to be displayed.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
